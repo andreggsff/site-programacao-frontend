6352c63efcd78a6244108b02cbf92c126946de82
andreggsff <gomesandregustavo@gmail.com>
2026-03-09 20:05:32 -0300

    Projeto de perfil para disciplina de Front-End

---
+++ app/components/Profile.js
@@ -0,0 +1,19 @@
+export default function Profile() {
+  return (
+    <div>
+      <img 
+        src="/foto.jpg"
+        alt="Minha foto"
+        width="200"
+      />
+
+      <h2>André Gustavo </h2>
+
+      <p>
+        Sou estudante do terceiro periodo de sistemas para internet, na UNICAP.
+        Gosto de aprender sobre programação e criar projetos digitais, 
+        Sou bem curioso e me divirto no processo.       
+      </p>
+    </div>
+  );
+}
