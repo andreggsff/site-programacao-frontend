313218e39d78a191d038620b4dd399acc4fffee8
andreggsff <gomesandregustavo@gmail.com>
2026-03-09 20:30:14 -0300

    Corrigindo caminho do Profile

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
-import Profile from "../components/Profile";
+import Profile from "./components/Profile";
 
 export default function Home() {
   return (
     <main>
       <h1>Site criado para a disciplina Programação Front-End</h1>
-
       <Profile />
     </main>
   );
